949caf0209552141d4b2e662ddad59f8703935c4
eidleweise <ben.woodward@gmail.com>
2020-11-03 13:44:08 +0000

    spot of refactoring

--- Base64Tool.py
+++ Base64Tool.py
@@ -1,13 +1,26 @@
 import base64
 
+encoding_type = 'utf-8'
+# encoding_type = 'ascii'
+
+
 def getMode():
     while True:
         print('B64 encode or decode')
         mode = input().lower()
-        if mode in 'encode e decode'.split():
+        if mode in 'encode e decode d'.split():
             return mode
         else:
             print('Enter either "encode" or "e" or "decode" or "d".')
+
+
+def is_read_file():
+    print('Do you want to read from a file, or an input string?')
+    mode = input().lower()
+    if mode in 'file f'.split():
+        return True
+    else:
+        return False
 
 
 def getMessage():
@@ -15,25 +28,51 @@ def getMessage():
     return input()
 
 
+def get_filename():
+    print('Enter the filename:')
+    return input()
+
+
 def encode(message):
-    message_bytes = message.encode('utf-8')
+    message_bytes = message.encode(encoding_type)
     base64_bytes = base64.b64encode(message_bytes)
-    base64_message = base64_bytes.decode('utf-8')
+    base64_message = base64_bytes.decode(encoding_type)
     return base64_message
 
 
 def decode(base64_message):
-    base64_bytes = base64_message.encode('utf-8')
+    base64_bytes = base64_message.encode(encoding_type)
     message_bytes = base64.b64decode(base64_bytes)
-    decoded = message_bytes.decode('utf-8')
+    decoded = message_bytes.decode(encoding_type)
     return decoded
 
 
-mode = getMode()
-message = getMessage()
-if mode[0] == 'd':
-    output = decode(message)
-else:
-    output = encode(message)
+def readFile(file_name):
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    return lines
 
-print(output)
+def readFileStripped(file_name):
+    lines = readFile(file_name)
+    stripped_lines = []
+    for line in lines:
+        stripped_lines.append(line.strip())
+    return stripped_lines
+
+mode = getMode()
+if is_read_file():
+    filename = get_filename()
+    for line in readFileStripped(filename):
+        if mode[0] == 'd':
+            output = decode(line)
+        else:
+            output = encode(line)
+    print(output)
+else:
+    message = getMessage()
+    if mode[0] == 'd':
+        output = decode(message)
+    else:
+        output = encode(message)
+    print(output)
+
--- rotN.py
+++ rotN.py
@@ -16,7 +16,18 @@ rot_alpha = rot(ascii_lowercase, ascii_uppercase)
 rotn_alpha_enc = rot_alpha(13)
 rotn_alpha_dec = rot_alpha(-13)
 
-rot_47 = rot('!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~PQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNO')
+
+def get_rot_47_chars():
+    global join
+    chars = []
+    for x in range(33, 127):
+        char = chr(x)
+        chars.append(char)
+    join = "".join(chars)
+    return join
+
+
+rot_47 = rot(get_rot_47_chars())
 rot_47_enc = rot_47(47)
 rot_47_dec = rot_47(-47)
 
@@ -26,7 +37,7 @@ rot_47_dec = rot_47(-47)
 # dec = rotn_alpha_dec(enc)
 # print(dec)
 
-# enc = rot47enc('Hello World') # Mjqqt Btwqi
-# print(enc)
-# dec = rot47dec(enc)
-# print(dec)
+enc = rot_47_enc('Hello World') # Mjqqt Btwqi
+print(enc)
+dec = rot_47_dec(enc)
+print(dec)
