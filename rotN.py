18cccdd4be8c3f7f5f0ba2c655d79eb3e45ebc0a
eidleweise <ben.woodward@gmail.com>
2020-11-03 11:26:21 +0000

    Adding Tools For Wishblend

--- rotN.py
+++ rotN.py
@@ -9,14 +9,24 @@ def rot(*symbols):
     return _rot
 
 
-#rot5_num = rot('0123456789')(5)
-#rot5_num('1234') # 6789
+rot5_num = rot('0123456789')(5)
+rot5_num('1234') # 6789
 
 rot_alpha = rot(ascii_lowercase, ascii_uppercase)
-rotn_alpha_enc = rot_alpha(27)
-rotn_alpha_dec = rot_alpha(-27)
+rotn_alpha_enc = rot_alpha(13)
+rotn_alpha_dec = rot_alpha(-13)
 
-enc = rotn_alpha_enc('Hello World') # Mjqqt Btwqi
-print(enc)
-dec = rotn_alpha_dec(enc)
-print(dec)
+rot_47 = rot('!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~PQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNO')
+rot_47_enc = rot_47(47)
+rot_47_dec = rot_47(-47)
+
+
+# enc = rotn_alpha_enc('Hello World') # Mjqqt Btwqi
+# print(enc)
+# dec = rotn_alpha_dec(enc)
+# print(dec)
+
+# enc = rot47enc('Hello World') # Mjqqt Btwqi
+# print(enc)
+# dec = rot47dec(enc)
+# print(dec)
