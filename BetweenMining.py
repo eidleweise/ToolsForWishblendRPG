c5e9f968074e134970e036a1da3d9f17fa2ebdfa
eidleweise <ben.woodward@gmail.com>
2020-11-18 18:36:26 +0000

    Adding in Safe Trading

--- BetweenMining.py
+++ BetweenMining.py
@@ -4,7 +4,7 @@ import random
 def get_value(in_val):
     if in_val in range(1, 6):
         return 200
-    if in_val in range(6, 16):
+    if in_val in range(5, 16):
         return 400
     if in_val in range(15, 26):
         return 600
@@ -29,10 +29,17 @@ def get_value(in_val):
 
 
 d100 = random.randint(1, 101)
-mining_bonus = 40 + 7 + 7 + 2
-cargo = 16
+cobra_mining_bonus = 40 + 7 + 7 + 2
+cobra_cargo = 16
+type6_mining_bonus = 45 + 14 + 7 + 2
+type6_cargo = 64
 planetary_knowledge = 3
 mining_yield = get_value(d100)
 
+
+mining_bonus = type6_mining_bonus
+cargo = type6_cargo
+
+
 mining_credits = ((mining_bonus + cargo) * planetary_knowledge) * mining_yield
-print(str(mining_credits))
+print(str(d100) + ": " + str(mining_credits))
---
+++ SafeTrading.py
@@ -0,0 +1,41 @@
+import random
+
+
+def get_value(in_val):
+    if in_val in range(1, 6):
+        return 500
+    if in_val in range(5, 16):
+        return 600
+    if in_val in range(15, 26):
+        return 700
+    if in_val in range(25, 36):
+        return 800
+    if in_val in range(36, 46):
+        return 900
+    if in_val in range(46, 76):
+        return 1000
+    if in_val in range(76, 81):
+        return 1100
+    if in_val in range(81, 86):
+        return 1200
+    if in_val in range(86, 91):
+        return 1300
+    if in_val in range(91, 96):
+        return 1400
+    if in_val in range(96, 101):
+        return 1500
+    else:
+        return -1
+
+
+d100 = random.randint(1, 101)
+asp_cargo = 80
+type6_cargo = 104
+trade_bonus = 2
+profit_per_ton = get_value(d100)
+
+cargo = asp_cargo
+
+
+trade_credits = profit_per_ton * trade_bonus * cargo
+print(str(d100) + ": profit_per_ton [" + str(profit_per_ton) + "] credits: " + str(trade_credits))
